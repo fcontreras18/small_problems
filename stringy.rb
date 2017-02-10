3529fc4ffd50e61446c172ae01e071e915161a07
Francisco Echeverria <fcontreras18@gmail.com>
2017-02-09 16:20:32 -0800

    Small problems adding apps

---
+++ stringy.rb
@@ -0,0 +1,18 @@
+# Write a method that takes one argument, a positive integer, and returns a string of alternating 1s and 0s, 
+# always starting with 1. The length of the string should match the given integer.
+
+def stringy(size)
+  numbers = []
+
+  size.times do |index|
+    number = index.even? ? 1:0
+    numbers << number
+  end
+
+  numbers.join
+end
+
+puts stringy(6) 
+puts stringy(9) 
+puts stringy(4) 
+puts stringy(7)
